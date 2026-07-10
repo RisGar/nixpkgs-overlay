6b1aa3b06149b8ccf4d9e214f817660e2ac10527
Rishab Garg <mail@rishab-garg.de>
2026-07-10 18:09:18 +0200

    feat: add nerdfonts cheatsheet

--- flake.nix
+++ flake.nix
@@ -1,82 +1,75 @@
 {
   description = "Overlay for nixpkgs";
-  outputs =
-    {
-      nixln-edit,
-      nvim-config,
-      print-cli-rs,
-      ocrtool-mcp,
-      ...
-    }:
-    {
-      overlays.default = final: prev: {
-        thaw = prev.callPackage ./pkgs/thaw.nix { };
-        mole-mac = prev.callPackage ./pkgs/mole-mac.nix { };
-        dragterm = prev.callPackage ./pkgs/dragterm.nix { };
-        walavave-trash-cli = prev.callPackage ./pkgs/walavave-trash-cli.nix { };
-        create-thesis = prev.callPackage ./pkgs/create-thesis.nix { };
-        ekctl = prev.callPackage ./pkgs/ekctl.nix { };
+  outputs = inputs: {
+    overlays.default = final: prev: {
+      thaw = prev.callPackage ./pkgs/thaw.nix { };
+      mole-mac = prev.callPackage ./pkgs/mole-mac.nix { };
+      dragterm = prev.callPackage ./pkgs/dragterm.nix { };
+      walavave-trash-cli = prev.callPackage ./pkgs/walavave-trash-cli.nix { };
+      create-thesis = prev.callPackage ./pkgs/create-thesis.nix { };
+      ekctl = prev.callPackage ./pkgs/ekctl.nix { };
 
-        logseq = (
-          let
-            pkgs' = import (fetchTarball {
-              url = "https://github.com/NixOS/nixpkgs/archive/ec0c722e017dfccbb2f66a8aafbe003320266d33.tar.gz";
-              sha256 = "0jws2i94asr1yish76799gmyw51dj98n8badq3snc8prifmsd3a5";
-            }) { system = prev.stdenv.hostPlatform.system; };
-          in
-          pkgs'.logseq
-        );
+      logseq = (
+        let
+          pkgs' = import (fetchTarball {
+            url = "https://github.com/NixOS/nixpkgs/archive/ec0c722e017dfccbb2f66a8aafbe003320266d33.tar.gz";
+            sha256 = "0jws2i94asr1yish76799gmyw51dj98n8badq3snc8prifmsd3a5";
+          }) { system = prev.stdenv.hostPlatform.system; };
+        in
+        pkgs'.logseq
+      );
 
-        signal-desktop = prev.signal-desktop.override {
-          withAppleEmojis = true;
-        };
+      signal-desktop = prev.signal-desktop.override {
+        withAppleEmojis = true;
+      };
 
-        nvim = prev.callPackage nvim-config {
-          jdks = with prev; [
-            jdk17
-            jdk21
-            jdk25
-          ];
-        };
+      nvim = prev.callPackage inputs.nvim-config {
+        jdks = with prev; [
+          jdk17
+          jdk21
+          jdk25
+        ];
+      };
 
-        print-cli-rs = prev.callPackage print-cli-rs { };
-        nixln-edit = prev.callPackage nixln-edit { };
-        ocrtool-mcp = prev.callPackage ocrtool-mcp { };
+      print-cli-rs = prev.callPackage inputs.print-cli-rs { };
+      nixln-edit = prev.callPackage inputs.nixln-edit { };
+      ocrtool-mcp = prev.callPackage inputs.ocrtool-mcp { };
+      nerdfont-cheatsheet = prev.callPackage inputs.nerdfont-cheatsheet { };
 
-        yaziPlugins = prev.yaziPlugins // {
-          macos-trash = prev.callPackage (
-            {
-              fetchFromGitHub,
-            }:
-            prev.yaziPlugins.mkYaziPlugin {
-              pname = "macos-trash.yazi";
-              version = "unstable-2026-06-22";
+      yaziPlugins = prev.yaziPlugins // {
+        macos-trash = prev.callPackage (
+          {
+            fetchFromGitHub,
+          }:
+          prev.yaziPlugins.mkYaziPlugin {
+            pname = "macos-trash.yazi";
+            version = "unstable-2026-06-22";
 
-              installPhase = ''
-                runHook preInstall
+            installPhase = ''
+              runHook preInstall
 
-                cp -r . $out
+              cp -r . $out
 
-                runHook postInstall
-              '';
+              runHook postInstall
+            '';
 
-              src = fetchFromGitHub {
-                owner = "walavave";
-                repo = "macos-trash.yazi";
-                rev = "130e6dd80d544b97016c877251dc7d51a0aac5a0";
-                hash = "sha256-A3nUll80LWWZcbX+2GjGUQA5lMbQPBwYKfOT+Sir24k=";
-              };
+            src = fetchFromGitHub {
+              owner = "walavave";
+              repo = "macos-trash.yazi";
+              rev = "130e6dd80d544b97016c877251dc7d51a0aac5a0";
+              hash = "sha256-A3nUll80LWWZcbX+2GjGUQA5lMbQPBwYKfOT+Sir24k=";
+            };
 
-              meta = {
-                description = "macOS trash plugin for Yazi";
-                homepage = "https://github.com/walavave/macos-trash.yazi";
-                license = prev.lib.licenses.mit;
-              };
-            }
-          ) { };
-        };
+            meta = {
+              description = "macOS trash plugin for Yazi";
+              homepage = "https://github.com/walavave/macos-trash.yazi";
+              license = prev.lib.licenses.mit;
+            };
+          }
+        ) { };
       };
     };
+  };
   inputs = {
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -95,6 +88,10 @@
     };
     ocrtool-mcp = {
       url = "github:RisGar/ocrtool-mcp";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nerdfont-cheatsheet = {
+      url = "github:RisGar/nerdfont_cheatsheet";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
