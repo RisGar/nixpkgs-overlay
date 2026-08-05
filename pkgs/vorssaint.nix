e9d3cca7cc23f2c5df0c94814637ec9d8c25776d
Rishab Garg <mail@rishab-garg.de>
2026-08-05 19:43:18 +0200

    chore(vorssaint): add package

---
+++ pkgs/vorssaint.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  stdenvNoCC,
+  undmg,
+  fetchurl,
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "vorssaint";
+  version = "3.3.1";
+
+  src = fetchurl {
+    url = "https://github.com/vorssaint/vorssaint-utils/releases/download/v${finalAttrs.version}/Vorssaint-${finalAttrs.version}.dmg";
+    hash = "sha256-Du2egEkRqHDGhwXl3jH3jn285AiVXvJX+NiObO49TVI=";
+  };
+
+  sourceRoot = ".";
+
+  nativeBuildInputs = [ undmg ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p "$out/Applications"
+    cp -r *.app "$out/Applications"
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Powerful menu bar manager for macOS";
+    homepage = "https://github.com/stonerl/Thaw";
+    license = lib.licenses.gpl3;
+    platforms = lib.platforms.darwin;
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+  };
+})
