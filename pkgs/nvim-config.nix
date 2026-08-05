9e1642707eefbcd275df41bb2a529b979c61b6f2
Rishab Garg <mail@rishab-garg.de>
2026-08-05 19:43:18 +0200

    chore(nvim): update

--- pkgs/nvim-config.nix
+++ pkgs/nvim-config.nix
@@ -1,23 +1,17 @@
 {
   callPackage,
   fetchFromGitHub,
-  jdk17,
-  jdk21,
-  jdk25,
 }:
 
 let
   src = fetchFromGitHub {
     owner = "RisGar";
     repo = "nvim-config";
-    rev = "fda516c79a707a3ebad302782ac1a37c87a69447";
-    hash = "sha256-E9DgzPk/uBKtKtHHb5PXBjGECbFF1A6l6xqG/VlBf3g=";
+    rev = "f455d629e3dc5b51c3893f64d6e4c7721469ddee";
+    hash = "sha256-B3DQn211m+EUTEax/xQC6f2rQW0NwC+WZfKDMy0IoSk=";
   };
 in
-callPackage "${src}/default.nix" {
-  jdks = [
-    jdk17
-    jdk21
-    jdk25
-  ];
-}
+(callPackage "${src}/default.nix" { }).overrideAttrs (old: {
+  pname = "nvim-config";
+  inherit src;
+})
