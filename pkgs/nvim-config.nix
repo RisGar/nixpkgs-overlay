31f589128e5d7b78cecea9831958edcc4e46e07d
Rishab Garg <mail@rishab-garg.de>
2026-08-05 19:43:18 +0200

    chore(nvim): update

--- pkgs/nvim-config.nix
+++ pkgs/nvim-config.nix
@@ -10,8 +10,8 @@ let
   src = fetchFromGitHub {
     owner = "RisGar";
     repo = "nvim-config";
-    rev = "67d87735a05f5c6f1397e0115a0c1538d4761a84";
-    hash = "sha256-LVR4wMAjDL8Lh3Snl3pnn3Cw9E33zj1/1gZY7fCyJCM=";
+    rev = "fda516c79a707a3ebad302782ac1a37c87a69447";
+    hash = "sha256-E9DgzPk/uBKtKtHHb5PXBjGECbFF1A6l6xqG/VlBf3g=";
   };
 in
 callPackage "${src}/default.nix" {
