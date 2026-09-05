e3dbd26a6eaaab6d45579d71555acc87baf16101
Rishab Garg <mail@rishab-garg.de>
2026-09-05 22:33:55 +0200

    fix(lightning): package name

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       homeManagerModules.default = ./modules/jellyfin-mpv-shim.nix;
       overlays.default = final: prev: {
         jellyfin-mpv-shim = prev.callPackage ./pkgs/jellyfin-mpv-shim { };
-        lightning = prev.callPackage ./pkgs/lightning { };
+        lightning-matrix-client = prev.callPackage ./pkgs/lightning-matrix-client { };
         thaw = prev.callPackage ./pkgs/thaw.nix { };
         mole-mac = prev.callPackage ./pkgs/mole-mac.nix { };
         dragterm = prev.callPackage ./pkgs/dragterm.nix { };
