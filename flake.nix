fa51e87beee88293206b25148df1f5f01794f6ab
Rishab Garg <mail@rishab-garg.de>
2026-09-06 00:45:49 +0200

    chore: remove unused pkgs

--- flake.nix
+++ flake.nix
@@ -22,7 +22,6 @@
         drucktool-utils = prev.callPackage ./pkgs/drucktool-utils.nix { };
         create-thesis = prev.lib.addMetaAttrs { mainProgram = "create-thesis"; } final.drucktool-utils;
         autonup = prev.lib.addMetaAttrs { mainProgram = "autonup"; } final.drucktool-utils;
-        ocrtool-mcp = prev.callPackage ./pkgs/ocrtool-mcp.nix { };
         nvim = prev.callPackage ./pkgs/nvim-config.nix { };
         nixln-edit = prev.callPackage ./pkgs/nixln-edit.nix { };
         print-cli-rs = prev.callPackage ./pkgs/print-cli-rs.nix { };
@@ -34,39 +33,6 @@
 
         signal-desktop = prev.signal-desktop.override {
           withAppleEmojis = true;
-        };
-
-        yaziPlugins = prev.yaziPlugins // {
-          macos-trash = prev.callPackage (
-            {
-              fetchFromGitHub,
-            }:
-            prev.yaziPlugins.mkYaziPlugin {
-              pname = "macos-trash.yazi";
-              version = "unstable-2026-06-22";
-
-              installPhase = ''
-                runHook preInstall
-
-                cp -r . $out
-
-                runHook postInstall
-              '';
-
-              src = fetchFromGitHub {
-                owner = "walavave";
-                repo = "macos-trash.yazi";
-                rev = "130e6dd80d544b97016c877251dc7d51a0aac5a0";
-                hash = "sha256-A3nUll80LWWZcbX+2GjGUQA5lMbQPBwYKfOT+Sir24k=";
-              };
-
-              meta = {
-                description = "macOS trash plugin for Yazi";
-                homepage = "https://github.com/walavave/macos-trash.yazi";
-                license = prev.lib.licenses.mit;
-              };
-            }
-          ) { };
         };
       };
 
